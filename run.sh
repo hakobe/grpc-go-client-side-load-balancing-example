a5ed142a9e9b7e6d1e5c577439f69b6c05d04b16
hakobe <hakobe@gmail.com>
2018-04-08 23:50:10 +0900

    runner script

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+./server/server -hostport 0.0.0.0:5000 &
+./server/server -hostport 0.0.0.0:5001 &
+./server/server -hostport 0.0.0.0:5002 &
+./server/server -hostport 0.0.0.0:5003 &
+
+trap 'kill $(jobs -p)' EXIT
+
+sleep 5
+
+time ./client/client \
+    -server localhost:5000 \
+    -server localhost:5001 \
+    -server localhost:5002 \
+    -server localhost:5003 \
